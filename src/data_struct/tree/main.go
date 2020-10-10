0aca3975a49e5d08565e9e4dc65ce83f66b80281
lanbing222 <lanbing222zhu@126.com>
2020-10-10 08:57:07 +0800

    avl

--- src/data_struct/tree/main.go
+++ src/data_struct/tree/main.go
@@ -1,32 +1,34 @@
 package main
 
-type Avltree struct{
-	value int
-	hight int
-	LeftNode *Avltree
+import "fmt"
+
+type Avltree struct {
+	value     int
+	hight     int
+	LeftNode  *Avltree
 	RightNode *Avltree
 }
 
-func (node * Avltree) FindPar(RootPar *Avltree,Root *Avltree) *Avltree{
-	if RootPar==nil{
+func (node *Avltree) FindPar(RootPar *Avltree, Root *Avltree) *Avltree {
+	if RootPar == nil {
 		return nil
 	}
-	if node.value>Root.value{
-		return node.FindPar(Root,Root.RightNode)
-	}else if node.value<Root.value{
-		return node.FindPar(Root,Root.LeftNode)
-	}else{
+	if node.value > Root.value {
+		return node.FindPar(Root, Root.RightNode)
+	} else if node.value < Root.value {
+		return node.FindPar(Root, Root.LeftNode)
+	} else {
 		return RootPar
 	}
 }
 
-func (node *Avltree) RightTor(Root *Avltree,NodeParent *Avltree) {
-	NodeGraP:=NodeParent.FindPar(nil,Root)
-	var TempNode * Avltree
-	if
+func (node *Avltree) RightTor(Root *Avltree, NodeParent *Avltree) {
+	NodeGraP := NodeParent.FindPar(nil, Root)
+	var TempNode *Avltree
+	fmt.Println(NodeGraP, TempNode)
 
 }
 
 func main() {
-	
+
 }

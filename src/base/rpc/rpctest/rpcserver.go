8f219128f0ba53a571bfce99a855d2a40b9edab0
DeepBlue <zhuralap@gmail.com>
2020-12-04 18:12:50 +0800

    rpctest

--- src/base/rpc/rpctest/rpcserver.go
+++ src/base/rpc/rpctest/rpcserver.go
@@ -3,6 +3,7 @@ package rpctest
 import (
 	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
 )
 
@@ -18,17 +19,27 @@ func (S *Server) Write(data []byte) error {
 	buf := make([]byte, 4+len(data))
 	fmt.Println(buf)
 	binary.BigEndian.PutUint32(buf[:4], uint32(len(data)))
-	copy(buf[4:],data)
+	copy(buf[4:], data)
 	n, err := S.Conn.Write(buf)
 	fmt.Println(n)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	//S.Conn.Write()
 	return nil
 }
 
+func (S *Server) Read() ([]byte, error) {
+	header := make([]byte, 4)
+	n, err := io.ReadFull(S.Conn, header)
+	fmt.Println(n)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(header)
+	datalen := binary.BigEndian.Uint32(header)
+	data := make([]byte, datalen)
+	io.ReadFull(S.Conn, data)
 
-func (S *Server)Read() ([]byte,error) {
-	buf:=
+	return data, nil
 }

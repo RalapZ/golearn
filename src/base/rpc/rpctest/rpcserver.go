11df125f399b4ad4fab8ff7a13a5de6b4d3bbc04
lanbing222 <lanbing222zhu@126.com>
2020-12-04 17:49:08 +0800

    data  clean

--- src/base/rpc/rpctest/rpcserver.go
+++ src/base/rpc/rpctest/rpcserver.go
@@ -18,17 +18,17 @@ func (S *Server) Write(data []byte) error {
 	buf := make([]byte, 4+len(data))
 	fmt.Println(buf)
 	binary.BigEndian.PutUint32(buf[:4], uint32(len(data)))
-	copy(buf[4:],data)
+	copy(buf[4:], data)
 	n, err := S.Conn.Write(buf)
 	fmt.Println(n)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	//S.Conn.Write()
 	return nil
 }
 
-
-func (S *Server)Read() ([]byte,error) {
-	buf:=
+func (S *Server) Read() ([]byte, error) {
+	buf := "test"
+	return []byte(buf), nil
 }

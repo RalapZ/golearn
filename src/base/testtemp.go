f377311e4d0d35450b2fe023a69f8d733140bee2
lanbing222 <lanbing222zhu@126.com>
2020-12-07 09:09:12 +0800

    data  clean

--- src/base/testtemp.go
+++ src/base/testtemp.go
@@ -1,39 +1,47 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"net/http"
-	_ "net/http/pprof"
+	"log"
+	"os"
+	"runtime/trace"
+	"sync"
 )
 
-const debug = true
-
-type a struct {
-	t int
-}
-
 func main() {
-	//io.Reader()
-	var buf *bytes.Buffer
-	if debug {
-		buf = new(bytes.Buffer) // enable collection of output
+	// 1. 创建trace持久化的文件句柄
+	f, err := os.Create("trace.out")
+	if err != nil {
+		log.Fatalf("failed to create trace output file: %v", err)
 	}
-	f(buf) // NOTE: subtly incorrect!
-	if debug {
-		// ...use buf...
-	}
-	http.ListenAndServe(":6060", nil)
-	//f1:=new(a)
-	//f2:=make([]a,5)
-	//fmt.Printf("%v",f1)
-	//fmt.Printf("%v",f2)
-}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatalf("failed to close trace file: %v", err)
+		}
+	}()
 
-// If out is non-nil, output will be written to it.
-func f(out io.Writer) {
-	// ...do something...
-	if out != nil {
-		out.Write([]byte("done!\n"))
+	// 2. trace绑定文件句柄
+	if err := trace.Start(f); err != nil {
+		log.Fatalf("failed to start trace: %v", err)
 	}
+	defer trace.Stop()
+
+	// 下面就是你的监控的程序
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 100; i++ {
+			_ = make([]int, 0, 20000)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 100; i++ {
+			_ = make([]int, 0, 10000)
+		}
+	}()
+
+	wg.Wait()
 }

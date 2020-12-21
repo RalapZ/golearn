7153a465dfdf534ca6f60526dcac79457863552a
DeepBlue <zhuralap@gmail.com>
2020-12-21 13:35:12 +0800

    rpctest

--- src/base/mysql/testmysql.go
+++ src/base/mysql/testmysql.go
@@ -8,14 +8,40 @@ import (
 
 var mysqlstr = "xconf:123456@tcp(10.10.8.150:3306)/xconf"
 
-func main() {
-	db, err := sql.Open("mysql", mysqlstr)
+type User struct {
+	Id   int
+	Name string
+	Sex  string
+}
+
+var conn *sql.DB
+
+func InitDB(sqlstr string) {
+	var err error
+	conn, err = sql.Open("mysql", sqlstr)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(db)
-	db.SetMaxIdleConns(10)
-	r, _ := db.Query(" show tables;")
-	fmt.Println(r.Scan())
+	err = conn.Ping()
+	if err != nil {
+		fmt.Println(err)
+	}
+	conn.SetMaxIdleConns(10)
+}
 
+func main() {
+	InitDB(mysqlstr)
+	sqlstring := "INSERT INTO user(id,name,sex) VALUES(2,'ralap1','male1')"
+	conn.Exec(sqlstring)
+
+	r, err1 := conn.Query(" select * from user;")
+	//conn.QueryRow()
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+	a := User{}
+	for r.Next() {
+		r.Scan(&a.Id, &a.Name, &a.Sex)
+		fmt.Println(a)
+	}
 }
